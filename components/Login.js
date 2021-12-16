8bde2291ca466ac6423010349b95b030675df070
Saeed <saeed_et90@yahoo.com>
2021-12-16 21:45:08 +1100

    moralis auth added

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
+import { useMoralis } from "react-moralis";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { authenticate } = useMoralis();
+  return (
+    <div className="bg-black relative text-white">
+      <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
+        <Image
+          src="https://i.imgur.com/WYAjt3T.jpeg"
+          height={200}
+          width={200}
+          className="rounded-full object-cover"
+        />
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg font-bold  p-5 active:bg-yellow-600 "
+        >
+          Login with MetaMask
+        </button>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://i.imgur.com/WYAjt3T.jpeg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import { useMoralis } from "react-moralis";
 import Login from "../components/Login";
 
 export default function Home() {
+  const { isAuthenticated } = useMoralis();
+
+  if (!isAuthenticated) return <Login />;
+
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Web3 Chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Login />
+      <h1>Welcome to the app</h1>
     </div>
   );
 }
