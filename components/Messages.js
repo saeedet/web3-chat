239259cf7990074a6e4eb6b9e5612abea7486a81
Saeed <saeed_et90@yahoo.com>
2021-12-18 21:03:26 +1100

    messages added

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,23 @@
 import React, { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
 
 const Messages = () => {
   const { user } = useMoralis();
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan("createdAt", new Date(Date.now() - 1000 * 60 * 15)),
+    [],
+    {
+      live: true,
+    }
+  );
 
+  console.log(data);
   const endOfMessagesRef = useRef(null);
   return (
     <div className="pb-56">
@@ -14,7 +27,11 @@ const Messages = () => {
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
-      <div>{/* Each Message */}</div>
+      <div className="space-y-10 p-4">
+        {data?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
       <div className="flex justify-center">
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
