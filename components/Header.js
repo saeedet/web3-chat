d7d2e0cb163f15733a4931826f0b960c6ecf2777
Saeed <saeed_et90@yahoo.com>
2021-12-18 22:24:24 +1100

    logout btn added

--- components/Header.js
+++ components/Header.js
@@ -9,14 +9,17 @@ const Header = () => {
   return (
     <div className="text-pink-500 sticky top-0 p-5 z-50 bg-black shadow-sm border-b-2 border-pink-700 ">
       <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
-        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid group">
           <Image
             src="https://i.imgur.com/WYAjt3T.jpeg"
             layout="fill"
             objectFit="cover"
-            className="rounded-full hover:opacity-80 cursor-pointer"
+            className="rounded-full hover:opacity-80 cursor-pointer "
             onClick={logout}
           />
+          <div className="absolute -bottom-5 left-1/2 transform -translate-x-1/2 hidden group-hover:inline cursor-pointer">
+            Logout
+          </div>
         </div>
         <div className="text-left lg:text-center col-span-4">
           <div className="h-48 w-48 relative lg:mx-auto border-pink-500 border-8 rounded-full">
